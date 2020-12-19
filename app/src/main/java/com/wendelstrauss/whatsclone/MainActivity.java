690d198a36126f9ad74ae06499f0e87373d1a5ba
wendelstrauss <wendelstrauss@users.noreply.github.com>
2020-12-19 01:15:37 -0300

    initial point

--- app/src/main/java/com/wendelstrauss/whatsclone/MainActivity.java
+++ app/src/main/java/com/wendelstrauss/whatsclone/MainActivity.java
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.menu_configuracoes) {
             return true;
         }
 
